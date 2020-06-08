193442e703e70ab46e18ab64d9357f8d47d8b678
shadman-a <shadman@outlook.com>
2020-06-08 07:42:15 -0400

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,34 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def cars
+    Car.all.select do |car|
+      car.mechanic == @name
+    end
+  end
+
+  def car_owners
+    cars.map do |car|
+      car.owner
+    end
+  end
+
+  def car_owners_names
+    car_owners.map do |owner|
+      owner.name
+    end
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,50 @@ def reload
   load 'config/environment.rb'
 end
 
+class Car
+
+  attr_reader :make, :model, :classification
+  attr_accessor :owner, :mechanic
+
+  @@all = []
+
+  def initialize(make, model, classification, owner, mechanic)
+    @make = make
+    @model = model
+    @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    @@all.map do |car|
+      car.classification
+    end
+  end
+
+  def self.find_mechanics(classification)
+    Mechanic.all.select do |mechanic|
+      mechanic.specialty == classification
+    end
+  end
+
+end
+
+owner1 = CarOwner.new("Bob")
+owner2 = CarOwner.new("John")
+
+mechanic1 = Mechanic.new("Carol", "sports")
+mechanic2 = Mechanic.new("Garol", "econo")
+mechanic3 = Mechanic.new("Moe", "electric")
+
+car1= Car.new("BMW", "M5","sports", "Bob", "Carol")
+car2= Car.new("Toyota", "Corolla", "electric", "John", "Moe")
+car3= Car.new("Tesla", "Model3", "electric", "John", "Moe")
+
+
 binding.pry
